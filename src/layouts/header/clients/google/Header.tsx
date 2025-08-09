8784c035b15be9a09292ff681d76edfefe464adf
peamexx <peamexx@gmail.com>
2025-08-09 12:01:04 +0900

    feat: 커스텀 header google용 추가

---
+++ src/layouts/header/clients/google/Header.tsx
@@ -0,0 +1,24 @@
+
+import styles from './header.module.css';
+import logoImg from './logo.svg';
+
+import { Avatar } from 'primereact/avatar';
+import { Button } from 'primereact/button';
+
+function Header() {
+  return (
+    <div className={styles.header}>
+      <Button icon="pi pi-cog" rounded text severity="secondary" aria-label="설정" />
+      <div className={styles.logo}>
+        <img className={styles.logoIcon} src={logoImg} alt="" />
+        Upgrade your skills
+      </div>
+      <div className={styles.userWrap}>
+        <Avatar icon="pi pi-user" shape="circle" label="A" style={{ backgroundColor: '#2196F3', color: '#ffffff' }}></Avatar>
+        <Button icon="pi pi-sign-out" rounded text severity="secondary" aria-label="로그아웃" />
+      </div>
+    </div>
+  )
+}
+
+export default Header
