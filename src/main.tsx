d89fe51cdde608a2c8eb8f7ba12defa8fbebe591
peamexx <peamexx@gmail.com>
2025-08-09 16:05:45 +0900

    feat: 라우터 추가

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
 import { PrimeReactProvider } from 'primereact/api';
+import { RouterProvider } from "react-router";
+import { router } from './routes/router';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <PrimeReactProvider>
-      <App />
+      <RouterProvider router={router} />
     </PrimeReactProvider>
   </StrictMode>,
 )
---
+++ src/routes/router.tsx
@@ -0,0 +1,35 @@
+import { createBrowserRouter } from "react-router";
+import App from '../App';
+// import BookList from '../layouts/books/book-list/BookList'
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    Component: App,
+    children: [
+      {
+        path: "/book",
+        // Component: Book,
+        children: [
+          // { path: "list", Component: BookList },
+        ],
+      },
+      // {
+      //   path: "auth",
+      //   Component: AuthLayout,
+      //   children: [
+      //     { path: "login", Component: Login },
+      //     { path: "register", Component: Register },
+      //   ],
+      // },
+      // {
+      //   path: "concerts",
+      //   children: [
+      //     { index: true, Component: ConcertsHome },
+      //     { path: ":city", Component: ConcertsCity },
+      //     { path: "trending", Component: ConcertsTrending },
+      //   ],
+      // },
+    ],
+  },
+]);
