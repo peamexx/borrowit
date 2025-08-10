79a40bbb2af0d41e63869b64dae106d3ae9e5fec
peamexx <peamexx@gmail.com>
2025-08-10 15:08:37 +0900

    feat: vite 레이아웃 플러그인 주석 및 이름 수정

--- src/utils/autoLayoutPlugin.ts
+++ src/utils/autoLayoutPlugin.ts
@@ -3,20 +3,19 @@ import path from 'path';
 
 export const createAutoLayoutPlugin = (CLIENT: string) => {
   const layoutsDir = path.resolve('./', 'src/layouts');
-  console.debug('layoutsDir',layoutsDir);
   const components = fs.readdirSync(layoutsDir, { withFileTypes: true })
-  .filter(dirent => dirent.isDirectory())
-  .map(dirent => dirent.name);
-  console.debug('components',components);
+    .filter(dirent => dirent.isDirectory())
+    .map(dirent => dirent.name);
+  console.debug('AutoLayoutPlugin: ', components);
 
   return {
-    name: 'auto-component-resolver',
+    name: 'auto-layout-resolver',
     resolveId(id: any) {
       const match = id.match(/^virtual:client-(.+)$/);
       if (match) {
         const componentName = match[1];
-        console.debug('componentName',componentName);
-        
+        console.debug('componentName', componentName);
+
         if (components.includes(componentName)) {
           const clientPath = path.resolve(layoutsDir, `${componentName}/clients/${CLIENT}/${capitalize(componentName)}.tsx`);
           const globalPath = path.resolve(layoutsDir, `${componentName}/global/${capitalize(componentName)}.tsx`);
