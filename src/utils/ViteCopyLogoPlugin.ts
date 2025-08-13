1dfbc4b86d9f4d49b176343ab6713880046f528f
peamexx <peamexx@gmail.com>
2025-08-13 18:34:49 +0900

    feat: 로고파일복사 플러그인에서 폴더삭제 추가, 주석 변경

--- src/utils/ViteCopyLogoPlugin.ts
+++ src/utils/ViteCopyLogoPlugin.ts
@@ -4,7 +4,7 @@ import path from 'path';
 /**
  * 개발: public/logo/.../에 있는 logo.png 중 CLIENT에 맞는 폴더에서 꺼내 public/logo.png로 생성.
  * 
- * 빌드: public/logo/.../에 있는 logo.png 중 CLIENT에 맞는 폴더에서 꺼내 dist/logo.png로 생성.
+ * 빌드: public/logo/.../에 있는 logo.png 중 CLIENT에 맞는 폴더에서 꺼내 dist/logo.png로 생성 및 dist/logo폴더 제거.
  * @param CLIENT env에 써진 VITE_CLIENT
  * @param mode 개발 또는 빌드 모드
  * @returns logo 파일 
@@ -19,15 +19,25 @@ export const copyLogoPlugin = (CLIENT: string, mode: 'DEV' | 'BUILD') => {
       srcPath = path.resolve(logoDir, `logo/clients/${CLIENT}/logo.png`);
     }
 
-    const distLogoPath = path.resolve('./', `${dest}/logo.png`)
 
     if (!fs.existsSync(path.resolve('./', `${dest}`))) {
-      console.error('dist 폴더가 존재하지 않습니다.')
-      return
+      console.error('dist 폴더가 존재하지 않습니다.');
+      return;
     }
 
-    fs.copyFileSync(srcPath, distLogoPath)
-    console.log(`Copied ${srcPath} to ${distLogoPath}`)
+    const distLogoPath = path.resolve('./', `${dest}/logo.png`);
+    fs.copyFileSync(srcPath, distLogoPath);
+
+    if (mode === 'BUILD') {
+      const distLogoFolderPath = path.resolve('./', `${dest}/logo/`);
+      if (fs.existsSync(distLogoFolderPath)) {
+        fs.rm(distLogoFolderPath, { recursive: true }, (err) => {
+          console.error(err);
+        })
+      }
+    }
+
+    console.log(`✅ 복사 완료: ${distLogoPath}`);
   }
 
   if (mode === 'DEV') {
