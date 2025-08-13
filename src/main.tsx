323d9b33a4e0181a8b82110cebb86d6142887e11
peamexx <peamexx@gmail.com>
2025-08-13 17:56:36 +0900

    feat: StrictMode 제거

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,10 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { PrimeReactProvider } from 'primereact/api';
 import { RouterProvider } from "react-router";
 import { router } from './routes/router';
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
     <PrimeReactProvider>
       <RouterProvider router={router} />
     </PrimeReactProvider>
-  </StrictMode>,
 )
