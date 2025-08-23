48e3460cb02b908c645167a3f76c8b454cffd251
peamexx <peamexx@gmail.com>
2025-08-23 16:33:52 +0900

    feat: 고정 퀴즈 게임 1개 추가

---
+++ public/game/src/assets/data.js
@@ -0,0 +1,22 @@
+const gameData = [
+  {
+    id: 1,
+    html: '달팽이도 이빨이 있다.',
+    isCorrect: true,
+    answerHtml: '정답: O<br/>달팽이의 이빨은 치설이라 불리며<br/>약 2만개 정도 존재합니다.'
+  },
+  {
+    id: 2,
+    html: '딸기는 장미과에 속한다.',
+    isCorrect: true,
+    answerHtml: '정답: O<br/>딸기는 장미과의 다년생 식물입니다.'
+  },
+  {
+    id: 3,
+    html: '열대어가 입을 맞추는 것은 애정의 표현이다.',
+    isCorrect: false,
+    answerHtml: '정답: X<br/>입을 맞추는 행위는 수컷 물고기들의 싸움 행위입니다.'
+  }
+];
+
+export default gameData;
---
+++ public/game/src/assets/script.js
@@ -0,0 +1,322 @@
+import gameData from './data.js';
+
+const GAME_DATA = gameData;
+const UI = createUiModule();
+
+window.onload = function () {
+  let currentQuizIndex = 0;
+
+  // start
+  init();
+
+  // main functions
+  function init() {
+    UI.startBtn.el.addEventListener('click', gameStart, { once: true });
+    UI.resetBtn.el.addEventListener('click', reset, { once: true });
+    UI.closeBtn.el.addEventListener('click', () => window.parent.close());
+  }
+
+  function gameStart() {
+    console.debug('gameStart: 게임 시작');
+    UI.startBtn.fadeOut();
+
+    UI.startBtn.el.addEventListener('animationend', () => {
+      setupQuizRound();
+
+      UI.quizBox.fadeIn();
+    }, { once: true });
+  }
+
+  function setupQuizRound() {
+    UI.quizText.el.innerHTML = getCurrentQuizHtml();
+
+    if (currentQuizIndex === 0) {
+      UI.oxBtn.onOclick((e) => handleAnswerSubmit.call(e, true));
+      UI.oxBtn.onXclick((e) => handleAnswerSubmit.call(e, false));
+    }
+
+    UI.oxBtn.disable(false);
+  }
+
+  function handleAnswerSubmit(userAnswer) {
+    UI.oxBtn.disable(true);
+
+    const isCorrect = isAnswerCorrect(userAnswer);
+
+    UI.snapshotBg.fadeIn(isCorrect);
+
+    UI.quizBox.fadeOut();
+    UI.quizBox.el.addEventListener('animationend', () => {
+      setupQuizAnswer();
+
+      UI.answerBox.fadeIn();
+    }, { once: true });
+  }
+
+  function setupQuizAnswer() {
+    UI.answerText.el.innerHTML = getCurrentAnswerHtml();
+
+    if (currentQuizIndex === 0) {
+      UI.nextBtn.onclick(handleNextSubmit);
+    }
+
+    UI.nextBtn.disable(false);
+  }
+
+  async function handleNextSubmit() {
+    UI.nextBtn.disable(true);
+
+    const enableProcced = await setNextQuizIndex();
+    if (!enableProcced.success) return;
+
+    UI.snapshotBg.fadeOut();
+
+    UI.answerBox.fadeOut();
+    UI.answerBox.el.addEventListener('animationend', () => {
+      setupQuizRound();
+
+      UI.quizBox.fadeIn();
+    }, { once: true });
+  }
+
+  function endGame() {
+    UI.answerBox.fadeOut();
+    UI.snapshotBg.fadeOut();
+
+    setTimeout(() => {
+      UI.endBox.fadeIn();
+    }, 1000);
+  }
+
+  function reset() {
+    console.debug('reset: 게임 리셋');
+    document.body.style.pointerEvents = 'none';
+
+    // 변수 리셋
+    currentQuizIndex = 0;
+
+    // 이벤트 리셋
+    UI.oxBtn.offOclick();
+    UI.oxBtn.offXclick();
+    UI.nextBtn.offclick();
+
+    // animation 리셋
+    UI.endBox.fadeOut();
+    UI.snapshotBg.fadeOut();
+    UI.answerBox.fadeOut();
+    UI.quizBox.fadeOut();
+
+    // disable 리셋
+    UI.nextBtn.disable(false);
+    UI.oxBtn.disable(false);
+
+    setTimeout(() => {
+      document.body.removeAttribute('style');
+
+      init();
+
+      UI.startBtn.fadeIn();
+    }, 1000);
+  }
+
+  // etc functions
+  function getCurrentQuizHtml() {
+    return GAME_DATA[currentQuizIndex].html;
+  }
+
+  function isAnswerCorrect(userAnswer) {
+    return GAME_DATA[currentQuizIndex].isCorrect === userAnswer ? true : false;
+  }
+
+  function getCurrentAnswerHtml() {
+    return GAME_DATA[currentQuizIndex].answerHtml;
+  }
+
+  function setNextQuizIndex() {
+    return new Promise((res) => {
+      if (currentQuizIndex == GAME_DATA.length - 1) {
+        endGame();
+        res({ success: false, code: 'END' });
+      }
+
+      currentQuizIndex = currentQuizIndex + 1;
+      console.debug('게임 인덱스 증가: ', currentQuizIndex);
+      res({ success: true });
+    })
+  }
+}
+
+function createUiModule() {
+  const resetBtnEl = document.querySelector('#reset-btn');
+  const closeBtnEl = document.querySelector('#close-btn');
+  const startBtnEl = document.querySelector('#start-btn');
+
+  // 퀴즈 영역
+  const quizBoxEl = document.querySelector('#quiz-box');
+  const quizTextEl = quizBoxEl.querySelector('.quiz-text');
+  const oBtnEl = quizBoxEl.querySelector('.o-btn');
+  const xBtnEl = quizBoxEl.querySelector('.x-btn');
+
+  // 답안 영역
+  const answerBoxEl = document.querySelector('#answer-box');
+  const answerTextEl = answerBoxEl.querySelector('.quiz-text');
+  const nextBtnEl = answerBoxEl.querySelector('.next-btn');
+
+  // 포토카드같은 사진
+  const snapshotBgEl = document.querySelector('#snapshot-bg');
+
+  // 종료 영역
+  const endBoxEl = document.querySelector('#end-box');
+
+  const handlers = new Map();
+
+  return {
+    resetBtn: {
+      el: resetBtnEl
+    },
+    closeBtn: {
+      el: closeBtnEl
+    },
+    startBtn: {
+      el: startBtnEl,
+      fadeIn: () => {
+        startBtnEl.classList.remove('animate__fadeOut');
+        startBtnEl.classList.add('animate__repeat-3');
+      },
+      fadeOut: () => {
+        startBtnEl.classList.remove('animate__repeat-3');
+        startBtnEl.classList.add('animate__fadeOut');
+      }
+    },
+    quizBox: {
+      el: quizBoxEl,
+      fadeIn: () => {
+        quizBoxEl.classList.remove('hide');
+        quizBoxEl.classList.add('animate__fadeInUp');
+      },
+      fadeOut: () => {
+        quizBoxEl.classList.add('animate__fadeOut');
+        quizBoxEl.addEventListener('animationend', () => {
+          quizBoxEl.classList.remove('animate__fadeOut', 'animate__fadeInUp');
+          quizBoxEl.classList.add('hide');
+        }, { once: true });
+      }
+    },
+    quizText: {
+      el: quizTextEl
+    },
+    oxBtn: {
+      oBtnKey: 'oBtnEl',
+      xBtnKey: 'xBtnEl',
+      disable(type) {
+        if (type) {
+          oBtnEl.disabled = true;
+          xBtnEl.disabled = true;
+        } else {
+          oBtnEl.disabled = false;
+          xBtnEl.disabled = false;
+        }
+      },
+      onOclick(handler) {
+        if (handlers.get(this.oBtnKey)) {
+          oBtnEl.removeEventListener('click', handlers.get(this.oBtnKey));
+          return;
+        }
+        handlers.set(this.oBtnKey, handler);
+        oBtnEl.addEventListener('click', handlers.get(this.oBtnKey));
+      },
+      offOclick() {
+        if (handlers.get(this.oBtnKey)) {
+          oBtnEl.removeEventListener('click', handlers.get(this.oBtnKey));
+          handlers.delete(this.oBtnKey);
+        }
+      },
+      onXclick(handler) {
+        if (handlers.get(this.xBtnKey)) {
+          xBtnEl.removeEventListener('click', handlers.get(this.xBtnKey));
+          return;
+        }
+        handlers.set(this.xBtnKey, handler);
+        xBtnEl.addEventListener('click', handlers.get(this.xBtnKey));
+      },
+      offXclick() {
+        if (handlers.get(this.xBtnKey)) {
+          xBtnEl.removeEventListener('click', handlers.get(this.xBtnKey));
+          handlers.delete(this.xBtnKey);
+        }
+      }
+    },
+    answerBox: {
+      el: answerBoxEl,
+      fadeIn: () => {
+        answerBoxEl.classList.remove('hide');
+        answerBoxEl.classList.add('animate__fadeInUp');
+      },
+      fadeOut: () => {
+        answerBoxEl.classList.add('animate__fadeOut');
+        answerBoxEl.addEventListener('animationend', () => {
+          answerBoxEl.classList.remove('animate__fadeOut', 'animate__fadeInUp');
+          answerBoxEl.classList.add('hide');
+        }, { once: true });
+      }
+    },
+    answerText: {
+      el: answerTextEl
+    },
+    nextBtn: {
+      btnKey: 'nextBtnEl',
+      el: nextBtnEl,
+      disable(type) {
+        if (type) {
+          nextBtnEl.disabled = true;
+        } else {
+          nextBtnEl.disabled = false;
+        }
+      },
+      onclick(handler) {
+        if (handlers.get(this.btnKey)) {
+          nextBtnEl.removeEventListener('click', handlers.get(this.btnKey));
+          return;
+        }
+        handlers.set(this.btnKey, handler);
+        nextBtnEl.addEventListener('click', handlers.get(this.btnKey));
+      },
+      offclick() {
+        if (handlers.get(this.btnKey)) {
+          nextBtnEl.removeEventListener('click', handlers.get(this.btnKey));
+          handlers.delete(this.btnKey);
+        }
+      },
+    },
+    snapshotBg: {
+      el: snapshotBgEl,
+      fadeIn: (userAnswer) => {
+        snapshotBgEl.dataset.ox = userAnswer ? 'o' : 'x';
+
+        snapshotBgEl.classList.remove('hide');
+        snapshotBgEl.classList.add('animate__rollIn');
+      },
+      fadeOut: () => {
+        snapshotBgEl.classList.add('animate__rollOut');
+        snapshotBgEl.addEventListener('animationend', () => {
+          snapshotBgEl.classList.remove('animate__rollOut', 'animate__rollIn');
+          snapshotBgEl.classList.add('hide');
+        }, { once: true });
+      }
+    },
+    endBox: {
+      el: endBoxEl,
+      fadeIn: () => {
+        endBoxEl.classList.remove('hide');
+        endBoxEl.classList.add('animate__fadeInUp');
+      },
+      fadeOut: () => {
+        endBoxEl.classList.add('animate__fadeOut');
+        endBoxEl.addEventListener('animationend', () => {
+          endBoxEl.classList.remove('animate__fadeOut', 'animate__fadeInUp');
+          endBoxEl.classList.add('hide');
+        }, { once: true });
+      }
+    }
+  }
+}
